093a0d5f7e95a126ae44b3124cb3dae1b596942b
Keks <keks@htmlacademy.ru>
2021-03-06 12:28:06 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/letter.js
@@ -0,0 +1,23 @@
+const letter = document.querySelector('.modal-letter');
+const letterOpenButton = document.querySelector('.contacts__link');
+const letterCloseButton = letter.querySelector('.modal-close');
+const letterInputName = letter.querySelector('.letter-form__input--name');
+
+letterOpenButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  letter.classList.add('modal-show');
+  letterInputName.focus();
+});
+
+letterCloseButton.addEventListener('click', function(e) {
+  letter.classList.remove('modal-show');
+});
+
+window.addEventListener("keydown", function (e) {
+  if (e.keyCode === 27) {
+    if (letter.classList.contains("modal-show")) {
+      e.preventDefault();
+      letter.classList.remove("modal-show");
+    }
+  }
+});
